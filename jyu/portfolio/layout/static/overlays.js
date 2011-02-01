f32d3fd7add510d0cba20670beb21d6a390d587f
Asko Soukka <asko.soukka@iki.fi>
2011-02-01 16:48:49 +0200

    Updates.

--- jyu/portfolio/layout/static/overlays.js
+++ jyu/portfolio/layout/static/overlays.js
@@ -1,18 +1,16 @@
 /*globals jQuery,common_content_filter,kukit*/
 jQuery(function($) {
   var init = function(el) {
-    try {
-      // autocomplete-plugin must be inited before placeholder-plugin
-      $(el).find("form div[id$=autocomplete]").autocomplete_z3cform();
-      $(el).find("form .field").placeholder_z3cform();
-    } catch (err) {
-      // Plugins above may not exist on all systems :)
-   };
+    // plugins below may not exist on all setups
+    try { $(el).find("form div[id$=autocomplete]").autocomplete_z3cform(); } catch (err) {};
+    // autocomplete-plugin must be inited before placeholder-plugin
+    try { $(el).find("form .field").placeholder_z3cform(); } catch (err) {};
     // Init KSS, but remember that KSS is made optional in 4.1!
     $(el).find("form").one("focus", function() {
       kukit.engine.setupEvents();
     });
   };
+  // Init overlay forms for tiles
   $('a.tile-add, a.tile-edit, a.tile-delete').each(function() {
     $($(this).prepOverlay({
       subtype: 'ajax',
@@ -29,7 +27,6 @@ jQuery(function($) {
       }
     }).attr("rel")).bind("onLoad", function() {
       init(this);
-    }).bind("onClose", function() {
     });
   });
 });
